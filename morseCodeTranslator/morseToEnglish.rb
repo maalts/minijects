7097f5c8dc125747c145b75e28104cb8bb52b4b3
maalts <coolmaxalt@gmail.com>
2015-11-25 23:47:51 +0000

    morse to english works, need to find a way to translate without so may if statements

--- morseCodeTranslator/morseToEnglish.rb
+++ morseCodeTranslator/morseToEnglish.rb
@@ -5,6 +5,7 @@ class MorseToEnglish
 	def initialize
 		@charArray = Array.new
 		@morseArray = Array.new
+		@output = ""
 		@x = 0
 	end
 	
@@ -15,7 +16,7 @@ class MorseToEnglish
 	
 	def translate
 		@charArray = @morse.split(" ")
-		puts @charArray
+# 		puts @charArray
 # 		@charArray.each do |c|
 # 			@morseArray << {@x => c.split("",5)}
 # 			@morseArray << [" ", " ", " ", " "]
@@ -27,11 +28,234 @@ class MorseToEnglish
 # 		end
 		@charArray.each do |c|
 			
+			if c == ".-"
+			
+				@output += ' a'
+			
+			end
+			
+			if c == "-..."
+			
+				@output += ' b'
+			
+			end
+			
+			if c == "-.-."
+			
+				@output += ' c'
+			
+			end
+			
+			if c == "-.."
+			
+				@output += ' d'
+			
+			end
+			
+			if c == "."
+			
+				@output += ' e'
+			
+			end
+			
+			if c == "..-."
+			
+				@output += ' f'
+			
+			end
+			
+			if c == "--."
+			
+				@output += ' g'
+			
+			end
+			
+			if c == "...."
+			
+				@output += ' h'
+			
+			end
+			
+			if c == ".."
+			
+				@output += ' i'
+			
+			end
+			
+			if c == ".---"
+			
+				@output += ' j'
+			
+			end
+			
+			if c == "-.-"
+			
+				@output += ' k'
+			
+			end
+			
+			if c == ".-.."
+			
+				@output += ' l'
+			
+			end
+			
+			if c == "--"
+			
+				@output += ' m'
+			
+			end
+			
+			if c == "-."
+			
+				@output += ' n'
+			
+			end
+			
+			if c == "---"
+			
+				@output += ' o'
+			
+			end
+			
+			if c == ".--."
+			
+				@output += ' p'
+			
+			end
+			
+			if c == "--.-"
+			
+				@output += ' q'
+			
+			end
+			
+			if c == ".-."
+			
+				@output += ' r'
+			
+			end
+			
+			if c == "..."
+			
+				@output += ' s'
+			
+			end
+			
+			if c == "-"
+			
+				@output += ' t'
+			
+			end
+			
+			if c == "..-"
+			
+				@output += ' u'
+			
+			end
+			
+			if c == "...-"
+			
+				@output += ' v'
+			
+			end
+			
+			if c == ".--"
+			
+				@output += ' w'
+			
+			end
+			
+			if c == "-..-"
+			
+				@output += ' x'
+			
+			end
+			
+			if c == "-.--"
+			
+				@output += ' y'
+			
+			end
+			
+			if c == "--.."
+			
+				@output += ' z'
+			
+			end
+			
+			if c == ".----"
+			
+				@output += ' 1'
+			
+			end
+			
+			if c == "..---"
+			
+				@output += ' 2'
+			
+			end
+			
+			if c == "...--"
+			
+				@output += ' 3'
+			
+			end
+			
+			if c == "....-"
+			
+				@output += ' 4'
+			
+			end
+			
+			if c == "....."
+			
+				@output += ' 5'
+			
+			end
+			
+			if c == "-...."
+			
+				@output += ' 6'
+			
+			end
+			
+			if c == "--..."
+			
+				@output += ' 7'
+			
+			end
+			
+			if c == "---.."
+			
+				@output += ' 8'
+			
+			end
+			
+			if c == "----."
+			
+				@output += ' 9'
+			
+			end
+			
+			if c == "-----"
+			
+				@output += ' 0'
+			
+			end
+			
+			if c == ""
+			
+				@output += ' '
+			
+			end
+			
 		end
+		
 	end
 	
 	def display
-		
+		puts @output
 	end
 	
 end
@@ -39,3 +263,4 @@ end
 translate = MorseToEnglish.new
 translate.read
 translate.translate
+translate.display
